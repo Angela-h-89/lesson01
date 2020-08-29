7f425f6deadf6a7d4e9a2ac0039e99705d358337
Angela-h-89 <angela-h-89@yandex.by>
2020-08-29 23:40:22 +0300

    hello

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x=2;
+        int y=4;
+        int result=x+y;
+	System.out.println(result);
+    }
+}
